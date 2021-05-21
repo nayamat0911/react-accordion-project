8ff4a5776b94e7ef150886a4e1d19498befbb430
nayamat ullah <nayamat0911@gmail.com>
2021-05-21 17:55:28 +0600

    create collapse app usesing react app

---
+++ src/components/Data.js
@@ -0,0 +1,19 @@
+export const Data =[
+    {
+        question:'what do you call a dog magician?',
+        Answer: 'A labracadabrodor lorem fhlafh  24G hdasfha  hahw'
+    },
+    {
+        question:'what do you call a dog magician?',
+        Answer: '1. A labracadabrodo '
+    },
+    {
+        question:'what do you call a dog magician?',
+        Answer: '2. labracadabrodor .'
+    },
+
+    {
+        question:'what do you call a dog magician?',
+        Answer: '3. labracadabrodor .'
+    },
+]
